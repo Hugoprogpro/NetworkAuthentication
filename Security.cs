32502bfdf2f63ed0d81b15225d1220d8849d0372
Guilherme Luiz Ferreira de Oliveira <guiferrolli@gmail.com>
2014-02-25 11:20:17 -0300

    Commiting initial class
    Basic logic. Olny to test

---
+++ Security.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Principal;
+using System.DirectoryServices.AccountManagement;
+namespace NetworkAuthentication
+{
+	public class Security
+	{
+		public static bool Authenticate(string userName, string Password){
+			String domainName = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+			bool isAuthenticated=false;
+			using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainName))
+			{
+				isAuthenticated = pc.ValidateCredentials(userName, Password);
+			}
+			return isAuthenticated;
+		}
+	}
+}
---
+++ User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using System.DirectoryServices.AccountManagement;
+namespace NetworkAuthentication
+{
+	public class User
+	{
+		public string UserName { get; private set; }
+		public string FullName { get; private set; }
+		public string FirstName { get; private set; }
+		public string LastName { get; private set; }
+		public User()
+		{
+			UserPrincipal userPrincipal= UserPrincipal.Current;
+			this.FullName = userPrincipal.DisplayName;
+			this.FirstName = userPrincipal.GivenName;
+			this.LastName = userPrincipal.Surname;
+			this.UserName = userPrincipal.EmailAddress;
+		}
+	}
+}
