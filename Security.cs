0b09ca1fe47e56324d153b015c4961fe8e621a21
Guilherme Luiz Ferreira de Oliveira <guiferrolli@gmail.com>
2014-03-12 17:34:25 -0300

    Sending Project Contents and Removing old stuff

---
+++ Security.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Principal;
+using System.DirectoryServices.AccountManagement;
+using System.Net;
+namespace NetworkAuthentication
+{
+	public class Security
+	{
+		public static string hostname;
+		public static bool Authenticate(string username, string password){
+			String domainName = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+			bool isAuthenticated=false;
+			using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainName))
+			{
+				isAuthenticated = pc.ValidateCredentials(username, password);
+			}
+			return isAuthenticated;
+		}
+		public static User GetUserByIpAddress(string ipAddress)
+		{	
+			System.Net.IPHostEntry ip = System.Net.Dns.GetHostEntry(ipAddress);
+			string userName = ip.HostName.Split('.')[0];
+			Security.hostname = userName;
+			return new User(userName);
+		}
+		public static User GetUserByUsername(string userName)
+		{
+			return new User(userName);
+		}
+	}
+}
---
+++ User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using System.DirectoryServices.AccountManagement;
+namespace NetworkAuthentication
+{
+	public class User
+	{
+		public string EmailAddress { get; private set; }
+		public string UserName { get; private set; }
+		public string FullName { get; private set; }
+		public string FirstName { get; private set; }
+		public string LastName { get; private set; }
+		public string Guid { get; private set; }
+		public User(string userName)
+		{
+			UserPrincipal userPrincipal;
+			String domainName = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+			using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainName))
+			{
+				userPrincipal = UserPrincipal.FindByIdentity(pc, userName);
+			}			
+			this.FullName = userPrincipal.DisplayName;
+			this.FirstName = userPrincipal.GivenName;
+			this.LastName = userPrincipal.Surname;
+			this.EmailAddress = userPrincipal.EmailAddress;
+			this.UserName = userName;
+			if (userPrincipal.Guid != null)
+			{
+				this.Guid = userPrincipal.Guid.Value.ToString();
+			}
+		}
+	}
+}
